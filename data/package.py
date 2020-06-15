2e2319e288e7c1f7b4a3e57fd890caf5d9422ec0
Coffey <lucascoffey@gmail.com>
2020-06-15 11:20:24 -0400

    Created SQLalchemy - data > package.py

---
+++ data/package.py
@@ -0,0 +1,23 @@
+import sqlalchemy as sa
+
+class Package:
+
+    id = sa.Column(sa.String, primary_key=True)
+    created_date = sa.Column(sa.DateTime)
+    summary = sa.Column(sa.String)
+    description = sa.Column(sa.String)
+
+    home_page = sa.Column(sa.String)
+    docs_url = sa.Column(sa.String)
+    package_url = sa.Column(sa.String)
+
+    author_name = sa.Column(sa.String)
+    author_email = sa.Column(sa.String)
+
+    # MAINTAINERS
+    # RELEASES
+
+    license = sa.Colum(sa.String)
+
+
+
